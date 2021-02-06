dbb1696711e7ecb91b8591dd9fd289ede778e2ef
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2021-02-06 08:09:11 -0700

    add navbar to application view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /events or /events.json
   def index
