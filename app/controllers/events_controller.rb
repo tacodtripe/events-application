788e9a809f438afd3990a62d1019ea5c9b697d5c
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2021-02-07 03:26:02 -0700

    add simple relation between models

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,7 @@ class EventsController < ApplicationController
   # GET /events or /events.json
   def index
     @events = Event.all
+    @event = Event.new
   end
 
   # GET /events/1 or /events/1.json
