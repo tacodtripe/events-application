33cad54149bf807f2433cea0c46bd50117b01f28
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2021-02-18 13:21:28 -0700

    created events helpers

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -10,6 +10,24 @@ module EventsHelper
   end
 
   def attend_confirmed(event)
-    Event.find_by(id: event.event_to_attend_id.to_s)[:title] if current_user.id == event.attendee_id
+    return unless current_user.id == event.attendee_id
+
+    Event.find_by(id: event.event_to_attend_id.to_s)
+  end
+
+  def attendance_fill
+    Attendance.all.each do |attendee|
+      next unless @event.id == attendee.event_to_attend_id
+
+      @v = User.find_by(id: attendee.attendee_id.to_s)[:username]
+      @attendance_list << @v
+      @v = ''
+    end
+  end
+
+  def attendance_confirm
+    return if @attendance_list.include?(current_user.username)
+
+    link_to 'Confirm attendance', new_event_attendance_path(@event)
   end
 end
