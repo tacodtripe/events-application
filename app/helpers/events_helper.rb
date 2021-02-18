7c53dcb1adf880fb4cc00549cfa9106395192bfe
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2021-02-18 11:33:33 -0700

    created delete_event and edit_event helpers

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,11 @@
 module EventsHelper
+  def edit_event(event)
+    link_to 'Edit', edit_event_path(event) if user_signed_in? && current_user.id == event.user_id
+  end
+
+  def delete_event(event)
+    return unless user_signed_in? && current_user.id == event.user_id
+
+    link_to 'Destroy', event, method: :delete, data: { confirm: 'Are you sure?' }
+  end
 end
